d76f860c086204bca5490a8984902ba29c5a4388
yalcinaksakal <yalcin.aksakal.tr@gmail.com>
2021-04-24 22:27:07 +0300

    react_forms

--- src/components/SimpleInput.js
+++ src/components/SimpleInput.js
@@ -1,9 +1,39 @@
-const SimpleInput = (props) => {
+import { useEffect, useState } from "react";
+
+const SimpleInput = props => {
+  const [name, setName] = useState("");
+  const [isNameTouched, setIsNameTouched] = useState(false);
+  
+  const isNameValid = name.trim() !== "";
+  const nameInputIsInvalid = !isNameValid && isNameTouched;
+
+  const nameInputChangeHandler = e => {
+    setName(e.target.value);
+  };
+
+  const nameInputBlurHandler = e => {
+    setIsNameTouched(true);
+  };
+  const formSubmitHandler = e => {
+    e.preventDefault();
+    setIsNameTouched(true);
+    if (!isNameValid) return;
+    setName("");
+    setIsNameTouched(false);
+  };
+ 
   return (
-    <form>
-      <div className='form-control'>
-        <label htmlFor='name'>Your Name</label>
-        <input type='text' id='name' />
+    <form onSubmit={formSubmitHandler}>
+      <div className={`form-control${nameInputIsInvalid ? " invalid" : ""}`}>
+        <label htmlFor="name">Your Name</label>
+        <input
+          type="text"
+          id="name"
+          onChange={nameInputChangeHandler}
+          onBlur={nameInputBlurHandler}
+          value={name}
+          placeholder={nameInputIsInvalid ? "Name must not be empty." : "Name"}
+        />
       </div>
       <div className="form-actions">
         <button>Submit</button>
