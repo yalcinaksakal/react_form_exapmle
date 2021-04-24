bb6b401c9f5df221d2fb39fb13bf4dff96cc6ce0
yalcinaksakal <yalcin.aksakal.tr@gmail.com>
2021-04-25 00:12:53 +0300

    react_forms

--- src/components/SimpleInput.js
+++ src/components/SimpleInput.js
@@ -1,42 +1,92 @@
-import { useEffect, useState } from "react";
+import {  useState } from "react";
+
+const emptyForm = {
+  name: {
+    value: "",
+    isTouched: false,
+    isValid: false,
+  },
+  email: {
+    value: "",
+    isTouched: false,
+    isValid: false,
+  },
+};
 
 const SimpleInput = props => {
-  const [name, setName] = useState("");
-  const [isNameTouched, setIsNameTouched] = useState(false);
-  
-  const isNameValid = name.trim() !== "";
-  const nameInputIsInvalid = !isNameValid && isNameTouched;
+  const [form, setForm] = useState(emptyForm);
+  const isFormValid = Object.values(form).reduce(
+    (acc, cur) => acc && cur.isValid,
+    true
+  );
+  const isNameInvalid = !form.name.isValid && form.name.isTouched;
+  const isEmailInvalid = !form.email.isValid && form.email.isTouched;
 
-  const nameInputChangeHandler = e => {
-    setName(e.target.value);
+  const nameChangeHandler = e => {
+    setForm({
+      ...form,
+      name: {
+        value: e.target.value,
+        isTouched: true,
+        isValid: e.target.value.trim() !== "",
+      },
+    });
+  };
+  const emailChangeHandler = e => {
+    setForm({
+      ...form,
+      email: {
+        value: e.target.value,
+        isTouched: true,
+        isValid: e.target.value.includes("@"),
+      },
+    });
   };
 
-  const nameInputBlurHandler = e => {
-    setIsNameTouched(true);
+  const nameBlurHandler = e => {
+    setForm({ ...form, name: { ...form.name, isTouched: true } });
+  };
+  const emailBlurHandler = e => {
+    setForm({ ...form, email: { ...form.email, isTouched: true } });
   };
   const formSubmitHandler = e => {
     e.preventDefault();
-    setIsNameTouched(true);
-    if (!isNameValid) return;
-    setName("");
-    setIsNameTouched(false);
+    setForm({
+      ...form,
+      name: { ...form.name, isTouched: true },
+      email: { ...form.email, isTouched: true },
+    });
+
+    if (!isFormValid) return;
+    setForm(emptyForm);
   };
- 
+
   return (
     <form onSubmit={formSubmitHandler}>
-      <div className={`form-control${nameInputIsInvalid ? " invalid" : ""}`}>
+      <div className={`form-control${isNameInvalid ? " invalid" : ""}`}>
         <label htmlFor="name">Your Name</label>
         <input
           type="text"
           id="name"
-          onChange={nameInputChangeHandler}
-          onBlur={nameInputBlurHandler}
-          value={name}
-          placeholder={nameInputIsInvalid ? "Name must not be empty." : "Name"}
+          onChange={nameChangeHandler}
+          onBlur={nameBlurHandler}
+          value={form.name.value}
+          placeholder={isNameInvalid ? "Name must not be empty." : "Name"}
+        />
+      </div>
+      <div className={`form-control${isEmailInvalid ? " invalid" : ""}`}>
+        <label htmlFor="email">Email</label>
+        <input
+          type="text"
+          id="email"
+          onChange={emailChangeHandler}
+          onBlur={emailBlurHandler}
+          value={form.email.value}
+          placeholder={isEmailInvalid ? "Please enter a valid email" : "Email"}
         />
       </div>
       <div className="form-actions">
-        <button>Submit</button>
+        <button disabled={!isFormValid}>Submit</button>
       </div>
     </form>
   );
